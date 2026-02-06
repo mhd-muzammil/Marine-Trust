19ac27e50a47e329ea03e1ca74c6e79ec34be989
Mhd-Muzammil <zamil627810@gmail.com>
2026-02-06 09:26:59 +0530

    #lib i

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ import daisyui from 'daisyui';
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // 👈 important
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
